0ea9f735eaf29027d5edafeec7b8fade875d3202
David Mansy <david.mansy@icloud.com>
2020-02-29 21:40:44 +0100

    Complete rendering exercice.

--- lessons/01-rendering/exercise/SignupForm.js
+++ lessons/01-rendering/exercise/SignupForm.js
@@ -1,12 +1,48 @@
-import React, { Fragment, useState } from "react"
+import React, { Fragment } from "react"
 import VisuallyHidden from "@reach/visually-hidden"
-import { signup } from "app/utils"
-import TabsButton from "app/TabsButton"
 import { FaDumbbell } from "react-icons/fa"
 import { DateFields, MonthField, DayField, YearField } from "app/DateFields"
-// import SignupForm from "./SignupForm.final"
-// export default SignupForm
+
+function TabsButton({ children }) {
+  return <button className="TabsButton icon_button cta">{children}</button>
+}
+
+function TextInput({ id, label, type = "text" }) {
+  return (
+    <Fragment>
+      <VisuallyHidden>
+        <label htmlFor={id}>{label}</label>
+      </VisuallyHidden>
+      <input
+        id={id}
+        type={type}
+        placeholder={label}
+        className="inputField"
+        required
+      />
+    </Fragment>
+  )
+}
 
 export default function SignupForm() {
-  return <form className="SignupForm">{/* Your work here */}</form>
+  return (
+    <form className="SignupForm">
+      <TextInput id="signup:displayName" label="Display name" />
+      <TextInput id="signup:photoUrl" label="Photo url" />
+      <TextInput id="signup:email" label="you@example.com" />
+      <TextInput id="signup:password" label="Password" type="password" />
+      <p>
+        <span>Start:</span>{" "}
+        <DateFields value={new Date()}>
+          <MonthField />
+          <DayField />
+          <YearField start={2016} end={2021} />
+        </DateFields>
+        <TabsButton>
+          <FaDumbbell />
+          <span>Signup</span>
+        </TabsButton>
+      </p>
+    </form>
+  )
 }
